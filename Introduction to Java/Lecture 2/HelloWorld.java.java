cdc065087a293830974e19a4deb64ee1200d6ddf
Kumar <kumarsumit1989@gmail.com>
2022-02-20 20:00:47 +0530

    Added Lecture 2 HelloWorld 20thFeb

---
+++ Introduction to Java/Lecture 2/HelloWorld.java.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+		//press ctrl+shift+f to format code		
+//		System.out.print("Kumar Sumit, this will be same line !");
+		System.out.println("This will be on new line after this, it gives a br after this!");
+		System.out.println("Hello World! new line");
+	}
+
+}
