725c5db3ac36115c0a3b6a5295430c20b293a4a5
Kumar Sumit <kumarsumit1989@gmail.com>
2022-02-22 11:10:31 +0530

    22Feb 2022 First Commit

--- Introduction to Java/Lecture 2/HelloWorld.java
+++ Introduction to Java/Lecture 2/HelloWorld.java
@@ -9,6 +9,15 @@ public class HelloWorld {
 //		System.out.print("Kumar Sumit, this will be same line !");
 		System.out.println("This will be on new line after this, it gives a br after this!");
 		System.out.println("Hello World! new line");
+                System.out.print("Hello World"); //print presents in same line
+		//here \n is used to display new line
+		System.out.println("\n");
+		//another way to print in new line
+		System.out.println("This is Kumar Sumit");
+		//after println new text will come in new line
+		System.out.println("this will come in new line");
+		//println gives a br after the new line
+		
 	}
 
 }
