ec6520bb0b6316b959e061735fb3c0717f3f459c
Kumar Sumit <kumarsumit1989@gmail.com>
2022-02-22 12:06:57 +0530

    Added InputOutPut.java

---
+++ Introduction to Java/Lecture 2/InputOutput.java
@@ -0,0 +1,20 @@
+package lecture2;
+import java.util.Scanner;
+
+public class InputOutput {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int a= s.nextInt(); //to take integer as input
+		System.out.println(a);
+		String str = s.next(); //to take first word as input.
+		System.out.println(str);
+		String str1 = s.nextLine(); //to take entire line as Input.
+		System.out.println(str1);
+		s.close();
+		
+	}
+
+}
