6b92c24de7fc7460f778cd5fad1f9d0b81e1b6ff
Kumar Sumit <kumarsumit1989@gmail.com>
2022-02-23 18:21:39 +0530

    23feb 2nd commit file change

--- Introduction to Java/Lecture 5, Patterns 1/lectureAssignmentsExercises/InterestingAlphabets.java
+++ Introduction to Java/Lecture 5, Patterns 1/lectureAssignmentsExercises/InterestingAlphabets.java
@@ -1,3 +1,44 @@
+/*Code : Interesting Alphabets
+
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+*/
+
+
+
+
 package lectureAssignmentsExercises;
 
 import java.util.Scanner;
